c15a4d57529471ced15d5de60093d3fcfb456616
Josh Robitaille <joshrobitaille@gmail.com>
2020-08-31 11:06:19 -0600

    commit: base page and connection working

--- src/main/java/com/josh/web/Firewall.java
+++ src/main/java/com/josh/web/Firewall.java
@@ -1,9 +1,13 @@
 package com.josh.web;
 
 import java.io.File;
-
+import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.WebResourceSet;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
 
 public class Firewall {
 
@@ -14,27 +18,21 @@ public class Firewall {
     }
 
     public static void setupServlet() {
-        String servletContextPath = "/Firewall";
+        final String base = new File("./").getAbsolutePath();
         Tomcat firewallServer = new Tomcat();
-        firewallServer.setBaseDir(new File("target/tomcat/").getAbsolutePath());
+        firewallServer.setBaseDir(new File("Firewall/target/tomcat/").getAbsolutePath());
         firewallServer.setPort(8989);
         firewallServer.getConnector();
-        firewallServer.addWebapp(servletContextPath, new File("src/main/resources/").getAbsolutePath());
-        firewallServer.addServlet(servletContextPath, "FirewallServlet", new FirewallServlet()).addMapping("/wall-of-fire");
+        Context context = firewallServer.addWebapp("", base);
+        WebResourceRoot resources = new StandardRoot(context);
+        WebResourceSet resourceSet = new DirResourceSet(resources, "/WEB-INF/classes", base, "/");
+        resources.addPreResources(resourceSet);
+        context.setResources(resources);
         try {
             firewallServer.start();
+            firewallServer.getServer().await();
         } catch (LifecycleException e) {
             System.err.println("Could not start firewall servlet");
         }
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            @Override
-            public void run() {
-                try {
-                    firewallServer.stop();
-                } catch (LifecycleException e) {
-                    System.err.println("Could not shut down firewall servlet");
-                }
-            }
-        });
     }
 }
--- src/main/java/com/josh/web/FirewallServlet.java
+++ src/main/java/com/josh/web/FirewallServlet.java
@@ -1,6 +1,8 @@
 package com.josh.web;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,10 +16,30 @@ public class FirewallServlet extends HttpServlet {
     public void init() throws ServletException {
         super.init();
     }
-    
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Firewall");
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter writer = resp.getWriter();
+
+        try {
+            writer.println("<!Doctype html>");
+            writer.println("<html>");
+            writer.println("<head>");
+            writer.println("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
+            writer.println("<title> Firewall </title>");
+            writer.println("</head>");
+            writer.println("<body>");
+            writer.println("<h2> Firewall Demo </h2>");
+            String img = "Firewall/Fire.jpg";
+            String alt = "Flame";
+            writer.println("<img src=" + img + " alt=" + alt + ">");
+            writer.println("</body>");
+            writer.println("</html>");
+        } finally {
+            writer.close();
+        }
+
     }
 
     @Override
